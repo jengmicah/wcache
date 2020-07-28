75d239a7230c9c4ff05c8a1f776c9a36ae938038
mjeng2 <mjeng2@illinois.edu>
2020-07-28 00:55:38 -0400

    Added namespace support

--- README.md
+++ README.md
@@ -1,42 +1,50 @@
 # webcache
 
-A flexible web storage cache with automatic key expiry.
+A flexible web storage cache with automatic key expiry and namespaces.
 
 ## Features
 
 - Easy data access and modification
+  - Query by key and by value
 - Automatic key expiry with a customizable cleanup interval
 - Configurable storage (`sessionStorage` and `localStorage`)
+- Segmented shared storage using namespaces
+  - Note that `localStorage` is unique per origin (`protocol://host:port`)
 
 ## Documentation
 
 The configurable parameters upon initialization are the following:
 
-1. `store`: type of web storage (default: `sessionStorage`)
-2. `cleanupTimer`: frequency of key expiry cleanup in seconds (default: `15`)
+1. `store`: type of web storage (`sessionStorage`/`localStorage`)
+2. `cleanupTimer`: frequency of key expiry cleanup in seconds
+3. `namespace`: namespace for this web store
+4. `allowDuplicateNamespaces`: whether to force unique namespaces
 
 ```js
+// defaults
 const cache = webcache({
-  store: "localStorage",
-  cleanupTimer: 30,
+  store: "sessionStorage",
+  cleanupTimer: 15,
+  namespace: "ns1",
+  allowDuplicateNamespaces: true,
 });
 ```
 
 The cache itself can be modified with the following functions:
 
 ```js
-cache.get(key);                            // returns parsed data stored under key
-cache.getAll();                            // returns all stored key/value pairs as an object
-cache.set(key, value[, overwrite[, ttl]]); // assigns key to value with expire time in seconds
-cache.setAll(data[, overwrite[, ttl]]);    // assigns all keys to values inside the data object
-cache.removeKey(key);                      // removes entries with the key, returns the value
-cache.removeValue(value);                  // removes entries with the value, returns list of keys
-cache.size();                              // returns number of keys
-cache.keys();                              // returns list of keys
-cache.has(key);                            // returns true or false
-cache.clear();                             // clear all items from storage
-cache.cleanup();                           // manually removes all expired entries
-cache.iterate((k, v) => {});               // call function on each key/value pair, return false to exit
+cache.get(key);                      // returns parsed data stored under key
+cache.getAll();                      // returns all stored key/value pairs as an object
+cache.set(data[, overwrite[, ttl]]); // assigns all keys to values in the data object with expire time in seconds
+cache.removeKey(key);                // removes entries with the key, returns the value
+cache.removeValue(value);            // removes entries with the value, returns list of keys
+cache.iterate((k, v) => {});         // call function on each key/value pair, return false to exit
+cache.size();                        // returns number of keys
+cache.keys();                        // returns list of keys
+cache.has(key);                      // returns true or false
+cache.clear();                       // clear all items from storage in this namespace
+cache.clearAll();                    // clear all items from storage (not namespace sensitive)
+cache.cleanup();                     // manually removes all expired entries
 ```
 
 ## Examples
@@ -52,11 +60,12 @@ const cache = webcache({
 
 ```js
 // Set item that lasts forever
-cache.set("key1", "forever");
+cache.set({ key1: "forever" });
 cache.get("key1"); // "forever"
 
 // Set item with 5 second expiry and overwrites previous value
-cache.set("key1", "only15", true, 5);
+cache.set({ key1: "only5" }, true, 5);
+cache.get("key1"); // "only5"
 // Wait 10 seconds
 setTimeout(() => {
   cache.get("key1"); // null
@@ -71,7 +80,7 @@ const data = {
   key2: "value2",
 };
 // Set multiple items with same 5 second expiry (overwriting any conflicts)
-cache.setAll(data, true, 5);
+cache.set(data, true, 5);
 // Get all items before they expire
 cache.getAll(); // { key1: "value1", key2: "value2" }
 ```
@@ -79,10 +88,16 @@ cache.getAll(); // { key1: "value1", key2: "value2" }
 ### `removeKey()` and `removeValue()`
 
 ```js
+const data = {
+  key1: "value1",
+  key2: "value2",
+};
+// Set multiple items (overwriting any conflicts)
+cache.set(data, true);
 // Remove entry by key
 cache.removeKey("key1"); // "value1"
 // Remove entry by value
-cache.removeValue("value2"); // "key2"
+cache.removeValue("value2"); // ["key2"]
 // Get value from nonexistent key
 cache.get("key1"); // null
 ```
@@ -97,12 +112,27 @@ const data = {
   key4: "value4",
 };
 // Set multiple items that last forever
-cache.setAll(data);
-// Run function until after value equals "value2"
+cache.set(data);
+// Run function until after value equals "value3"
+// Order is not guaranteed in web storage
 cache.iterate((k, v) => {
   console.log(`${k}: ${v}`);
-  if (v === "value2") return false;
+  if (v === "value3") return false;
 });
-// key1: value1
-// key2: value2
+```
+
+### `size()`, `keys()`, `has()`, `clear()`
+
+```js
+const data = {
+  key1: "value1",
+  key2: "value2",
+};
+// Set multiple items that last forever
+cache.set(data);
+cache.size(); // 2
+cache.keys(); // ["key1", "key2"]
+cache.has("key3"); // false
+cache.clear();
+cache.size(); // 0
 ```
--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-
-const isSupported = () => typeof (Storage) !== "undefined";
-const isAvailable = (store) => {
-    return !!store
-        && typeof store.getItem === 'function'
-        && typeof store.setItem === 'function'
-        && typeof store.removeItem === 'function';
-}
-
 module.exports = function ({
     store = 'sessionStorage',
-    cleanupTimer = 15
+    cleanupTimer = 15,
+    namespace = 'ns1',
+    allowDuplicateNamespaces = true
 }) {
-
     const storage = window[store];
+
+    const isSupported = () => typeof (Storage) !== "undefined";
+    const isAvailable = (store) => {
+        return !!store
+            && typeof store.getItem === 'function'
+            && typeof store.setItem === 'function'
+            && typeof store.removeItem === 'function';
+    }
+    const isValidNamespace = () => {
+        if (namespace.includes('.')) {
+            console.error(`Namespace "${namespace}" cannot contain a period.`);
+            return false;
+        }
+        for (const namespaced_key in { ...storage }) {
+            if (namespaced_key.substring(0, namespaced_key.indexOf('.')) === namespace && !allowDuplicateNamespaces) {
+                console.error(`Namespace "${namespace}" is a duplicate. Either change the namespace or set allowDuplicateNamespaces to true.`);
+                return false;
+            }
+        }
+        return true;
+    }
 
     if (!isSupported()) {
         console.error('Web storage is not supported on this browser.');
@@ -20,66 +33,94 @@ module.exports = function ({
     } else if (!isAvailable(storage)) {
         console.error(`Web store "${store}" is not available.`);
         return null;
+    } else if (!isValidNamespace()) {
+        return null;
     }
-    const get = (key) => {
-        const item = JSON.parse(storage.getItem(key));
-        if (item) {
-            if (item.expiry && Number(new Date()) > item.expiry) return null;
-            return item.value;
-        }
+
+    const namespaced = (key) => `${namespace}.${key}`;
+    const unnamespaced = (namespaced_key) => namespaced_key.substring(namespaced_key.indexOf('.') + 1)
+    const getNamespacedKey = (namespaced_key) => {
+        const item = JSON.parse(storage.getItem(namespaced_key));
+        if (item && (item.expiry === null || Number(new Date()) <= item.expiry))
+            return item.value
+        return null
     }
-    const getAll = () => {
-        let all = {};
-        for (const key in { ...storage })
-            all[key] = get(key);
-        return all;
-    }
-    const set = (key, value, overwrite = false, ttl = null) => {
-        if (!has(key) || overwrite) {
-            storage.setItem(key, JSON.stringify({
-                expiry: ttl ? Number(new Date()) + (ttl * 1000) : null,
-                value: value
-            }));
-        }
-    }
-    const setAll = (data, overwrite = false, ttl = null) => {
-        for (const [k, v] of Object.entries(data)) {
-            set(k, v, overwrite, ttl);
-        }
-    }
-    const removeKey = (key) => {
-        const val = get(key);
-        storage.removeItem(key);
+    const hasNamedspacedKey = (namespaced_key) => getNamespacedKey(namespaced_key) ? true : false;
+    const removeNamespacedKey = (namespaced_key) => {
+        const val = getNamespacedKey(namespaced_key);
+        storage.removeItem(namespaced_key);
         return val;
     }
+    const isInNamespace = (key) => key.substring(0, key.indexOf('.')) === namespace;
+    const loopNamespacedKeys = (fn) => {
+        for (const namespaced_key in { ...storage }) {
+            if (isInNamespace(namespaced_key)) {
+                const res = fn(namespaced_key);
+                if (!res && res !== undefined) break;
+            }
+        }
+    }
+
+    const get = (key) => getNamespacedKey(namespaced(key))
+    const getAll = () => {
+        let all = {};
+        loopNamespacedKeys((namespaced_key) => {
+            all[unnamespaced(namespaced_key)] = getNamespacedKey(namespaced_key);
+        });
+        return all;
+    }
+    const set = (data, overwrite = false, ttl = null) => {
+        for (const [key, value] of Object.entries(data)) {
+            const namespaced_key = namespaced(key)
+            if (!hasNamedspacedKey(namespaced_key) || overwrite) {
+                storage.setItem(namespaced_key, JSON.stringify({
+                    expiry: ttl ? Number(new Date()) + (ttl * 1000) : null,
+                    value: value
+                }));
+            }
+        }
+    }
+    const removeKey = (key) => removeNamespacedKey(namespaced(key));
     const removeValue = (val) => {
         let keys = [];
-        Object.keys(storage).map((k) => {
-            if (get(k) === val) {
-                keys.push(k);
-                removeKey(k);
+        loopNamespacedKeys((namespaced_key) => {
+            if (getNamespacedKey(namespaced_key) === val) {
+                keys.push(unnamespaced(namespaced_key));
+                removeNamespacedKey(namespaced_key);
             }
-        })
+        });
         return keys;
     };
-    const iterate = (fn) => {
-        for (const key in { ...storage }) {
-            const res = fn(key, get(key));
-            if (!res && res !== undefined) break;
-        }
+    const iterate = (user_fn) => {
+        loopNamespacedKeys((namespaced_key) => {
+            const unnamespaced_key = unnamespaced(namespaced_key);
+            const res = user_fn(unnamespaced_key, getNamespacedKey(namespaced_key));
+            if (!res && res !== undefined) return false;
+        });
     }
     const cleanup = () => {
         const now = Number(new Date());
-        for (const key in { ...storage }) {
-            const item = JSON.parse(storage.getItem(key));
+        loopNamespacedKeys((namespaced_key) => {
+            const item = JSON.parse(storage.getItem(namespaced_key));
             if (item && item.expiry && now > item.expiry)
-                removeKey(key);
-        };
+                removeNamespacedKey(namespaced_key);
+        });
     }
-    const keys = () => Object.keys(storage);
+    const keys = () => {
+        let keys = [];
+        loopNamespacedKeys((namespaced_key) => {
+            keys.push(unnamespaced(namespaced_key));
+        });
+        return keys;
+    }
     const size = () => keys().length;
-    const has = (key) => get(key) ? true : false;
-    const clear = () => storage.clear();
+    const has = (key) => hasNamedspacedKey(namespaced(key));
+    const clear = () => {
+        loopNamespacedKeys((namespaced_key) => {
+            removeNamespacedKey(namespaced_key);
+        });
+    }
+    const clearAll = () => storage.clear();
 
     cleanup();
     setInterval(cleanup, cleanupTimer * 1000);
@@ -88,7 +129,6 @@ module.exports = function ({
         get,
         getAll,
         set,
-        setAll,
         removeKey,
         removeValue,
         cleanup,
@@ -97,5 +137,6 @@ module.exports = function ({
         size,
         has,
         clear,
+        clearAll
     };
 };
